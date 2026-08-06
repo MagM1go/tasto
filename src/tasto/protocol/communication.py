fde7e4a266f6d7e10f0ff1557a9bb33ac12c5a39
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-08-06 22:22:26 +0700

    feat(protocol): rewrite `Communication` logic

--- src/tasto/protocol/communication.py
+++ src/tasto/protocol/communication.py
@@ -1,89 +1,51 @@
 from collections import deque
-from enum import StrEnum
+from typing import final
 
+from tasto.protocol._readers import HTTPMessageReaderStrategy
+from tasto.protocol._state import CommunicationState, Role
+from tasto.protocol._writers import HTTPMessageWriterStrategy
+from tasto.protocol.api.buffer import ReceiveBufferContract
 from tasto.protocol.api.events import Event
-from tasto.protocol.http11._receive import _ReceiveBuffer
-
-from tasto.protocol.http11._events import ChunkBody, Finish, FinishHeaderSection, Header, HexChunkSize, MessageBodyEnd, MessageEnd, Request, Status
-
-
-class CommunicationRole(StrEnum):
-    CLIENT = "client"
-    SERVER = "server"
+from tasto.protocol.http11._buffer import Buffer
+from tasto.protocol.http11._events import Data, MessageEnd, Request, Response
 
 
-class _ConnectionState(StrEnum):
-    IDLE = "idle"
-    SEND_BODY = "send_body"
-    WAITING_RESPONSE = "waiting"
-    DONE = "done"
-
-
-STATES: dict[tuple[_ConnectionState, type[Event]], _ConnectionState] = {
-    (_ConnectionState.IDLE, Request): _ConnectionState.SEND_BODY,
-    (_ConnectionState.SEND_BODY, Finish): _ConnectionState.WAITING_RESPONSE,
-
-    (_ConnectionState.WAITING_RESPONSE, Status): _ConnectionState.WAITING_RESPONSE,
-    (_ConnectionState.WAITING_RESPONSE, Header): _ConnectionState.WAITING_RESPONSE,
-    (_ConnectionState.WAITING_RESPONSE, FinishHeaderSection): _ConnectionState.WAITING_RESPONSE,
-    (_ConnectionState.WAITING_RESPONSE, HexChunkSize): _ConnectionState.WAITING_RESPONSE,
-    (_ConnectionState.WAITING_RESPONSE, ChunkBody): _ConnectionState.WAITING_RESPONSE,
-    (_ConnectionState.WAITING_RESPONSE, MessageBodyEnd): _ConnectionState.WAITING_RESPONSE,
-    (_ConnectionState.WAITING_RESPONSE, MessageEnd): _ConnectionState.DONE
-}
-
-
-class _StateMachine:
-    def __init__(self) -> None:
-        self._state: _ConnectionState = _ConnectionState.IDLE
-
-    @property
-    def state(self) -> _ConnectionState:
-        return self._state
-
-    def transition(self, event: Event) -> None:
-        key = (self._state, type(event))
-        self._state = STATES[key]
-
-
+@final
 class Communication:
-    def __init__(self, role: CommunicationRole) -> None:
-        self._client_state = _StateMachine()
+    def __init__(self, role: Role) -> None:
         self._role = role
-        self._receive_buffer = _ReceiveBuffer()
-
+        self._state = CommunicationState(self._role)
         self._events: deque[Event] = deque()
 
-    @property
-    def my_role(self) -> CommunicationRole:
-        return self._role
+        self._writer_buffer: ReceiveBufferContract = Buffer()
+        self._reader_buffer: ReceiveBufferContract = Buffer()
 
-    @property
-    def side_role(self) -> CommunicationRole:
-        return next(filter(lambda x: x != self.my_role, CommunicationRole))
+        self._writer = HTTPMessageWriterStrategy(self._writer_buffer)
+        self._reader = HTTPMessageReaderStrategy(self._reader_buffer)
 
-    def _cleanup(self) -> None:
-        self._receive_buffer._buffer = bytearray()
+    def next_event(self) -> Event | None:
+        if not self._events:
+            return None
+
+        return self._events.popleft()
 
     def create_message(self, event: Event) -> bytes:
-        """Builds HTTP message like that:
+        self._state.transition(event)
+        self._events.append(event)
 
-        - https://www.rfc-editor.org/info/rfc9112/#name-message-format
+        if isinstance(event, Request):
+            self._writer.write_request(event)
 
-        Here's a snippet from RFC.
-        ```
-        HTTP-message   = start-line CRLF
-                         *( field-line CRLF )
-                         CRLF
-                         [ message-body ]
-        ```
-        """
-        self._client_state.transition(event)
+        elif isinstance(event, Response):
+            self._writer.write_response(event)
 
-        
+        elif isinstance(event, Data):
+            self._writer.write_chunk(
+                chunk_size=len(event.data),
+                chunk=event,
+            )
 
-    def signal_my_message_end(self) -> None:
-        self._client_state.transition(Finish())
+        elif isinstance(event, MessageEnd):
+            self._writer.write_eom()
 
-        
-        
+        return self._writer_buffer.consume()
