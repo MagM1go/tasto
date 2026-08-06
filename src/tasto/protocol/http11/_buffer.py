74ba7662cc0ca9f62af77a1ba3c598ea4693103a
MagM1go <68418026+MagM1go@users.noreply.github.com>
2026-08-06 22:23:44 +0700

    refactor(protocol): rename `_receive` -> `_buffer`, add `.consume()` method

--- src/tasto/protocol/http11/_buffer.py
+++ src/tasto/protocol/http11/_buffer.py
@@ -1,21 +1,27 @@
+from typing import final, override
+
 from tasto.protocol._exceptions import CRLFNotFound
 from tasto.protocol.api import ReceiveBufferContract
 from tasto.protocol.http11._abnf import CRLF
 
 
-class _ReceiveBuffer(ReceiveBufferContract):
+# да и ваще мне это не нравится
+@final
+class Buffer(ReceiveBufferContract):
     def __init__(self) -> None:
         self._crlf = CRLF.encode()
         self._buffer = bytearray()
 
     @property
+    @override
     def buffer(self) -> bytearray:
         return self._buffer
 
     @buffer.setter
-    def buffer(self) -> None:
+    def buffer(self) -> None:  # type: ignore[misc]
         raise NotImplementedError()
 
+    @override
     def is_empty(self) -> bool:
         return not self._buffer
 
@@ -25,28 +31,36 @@ class _ReceiveBuffer(ReceiveBufferContract):
     #   - And um whole https://www.rfc-editor.org/info/rfc9112/#section-11 ...
     #
     # !!!!!!!!!!!! THIS IS JUST PROOF OF WORK PURPOSES NOT PROD READY !!!!!!!!!!!!
+    @override
     def put_data(self, data: bytes) -> None:
         # if not data.endswith(2 * CRLF):
         #     data += 2 * CRLF
 
         self._buffer += data
 
+    @override
     def read_at_most(self, byte_count: int) -> bytes | None:
         data_chunk = self._buffer[:byte_count]
         if not data_chunk:
-            return
+            return None
 
         self._buffer = self._buffer[byte_count:]
         return bytes(data_chunk)
 
-    def read_until_crlf(self) -> bytes | None:
-        if self._crlf not in self._buffer:
-            return
-
+    # TODO: переписать.
+    @override
+    def read_until_crlf(self) -> bytes:
         line, buffer = self._buffer.split(self._crlf, 1)
         self._buffer = buffer
         return bytes(line)
 
+    @override
     def read_eom(self) -> None:
-        if self.read_at_most(2) != CRLF:
+        if self.read_at_most(2) != self._crlf:
             raise CRLFNotFound()
+
+    @override
+    def consume(self) -> bytes:
+        result = bytes(self._buffer)
+        self._buffer.clear()
+        return result
